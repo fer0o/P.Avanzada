c2ecf0dc55fcd5e09ccef10991cdb411f2a82bf7
Fernando <fer0osennin@gmail.com>
2018-05-04 17:52:08 -0500

    modificacion de proyecto03

--- proyectos/proyecto03/proyecto03.c
+++ proyectos/proyecto03/proyecto03.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-long long ultk(int n,int p,long long oper)
-{
+long long ultk(int n,int p,long long oper){
        long long res = 1, base = n;
        while (p) {
                if (p&1) {
@@ -12,8 +11,7 @@ long long ultk(int n,int p,long long oper)
        }
        return res;
 }
-long long primk(int n,int k)
-{
+long long primk(int n,int k){
        long double m = n*log10l(n);
        long double p = powl(10,m-(int)m);
        long long res = (long long)(p*pow(10,k));
@@ -21,12 +19,10 @@ long long primk(int n,int k)
        while(res>=value) res/=10;
        return res;
 }
-int main()
-{
+int main(){
        int N;
        scanf("%d",&N);
-       while(N--)
-       {
+       while(N--){
                int n,k;
                scanf("%d %d",&n,&k);
                long long oper = pow(10,k);
